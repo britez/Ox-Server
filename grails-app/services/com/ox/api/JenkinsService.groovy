6bee6ed2754a09c38b713285b8e08c6eada97c71
Maximiliano Britez <maximiliano.britez@globallogic.com>
2014-11-14 16:55:14 -0300

    Add Jenkins service

--- grails-app/services/com/ox/api/JenkinsService.groovy
+++ grails-app/services/com/ox/api/JenkinsService.groovy
@@ -1,11 +1,25 @@
 package com.ox.api
 
 import grails.transaction.Transactional
+import groovyx.net.http.ContentType
+import groovyx.net.http.HTTPBuilder
+import groovyx.net.http.HttpResponseDecorator
+import groovyx.net.http.Method
 
 @Transactional
 class JenkinsService {
+	
+	private static final String BASE_URL = "http://localhost:8888"
 
-    def serviceMethod() {
-
-    }
+    def createJob(){
+		HTTPBuilder builder = new HTTPBuilder(BASE_URL)
+		builder.request( Method.POST ) { 
+			uri.path = '/jenkins/createItem=?test'
+			requestContentType = ContentType.XML
+			body = "<project />"
+			response.success = { HttpResponseDecorator resp, xml ->
+				xmlResult = xml
+			}
+		}
+	}
 }
